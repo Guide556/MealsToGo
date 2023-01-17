79e1785175d508a9efa892d9b5f7c58c70008faf
Focus <64123250110@kru.ac.th>
2023-01-17 14:28:29 +0700

    SVG

--- src/features/restaurants/components/restaurant-info-card.component.jsx
+++ src/features/restaurants/components/restaurant-info-card.component.jsx
@@ -3,15 +3,19 @@ import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 import { Text } from "react-native";
 
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+
 // ถ้า styled ใช้ . ไม่ได้ให้ใช้ ()
 // props เป็นการเรียกใช้คุณสมบัติแบบ props โดยใช้ผ่าน ThemeProvider
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.secondary};
+  marginTop: 16px}
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-padding: ${(props) => props.theme.space[3]};
-background-color: white;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: white;
 `;
 
 const Title = styled(Text)`
@@ -29,6 +33,12 @@ const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
 export const RestaurantInfoCard = ({ ...restaurant }) => {
   const {
     name = "Some Restaurant",
@@ -36,9 +46,12 @@ export const RestaurantInfoCard = ({ ...restaurant }) => {
     photos = ["https://rerollcdn.com/GENSHIN/Items/NEW2/Victorious_Legend.png"],
     address = "100 some random street",
     isOpenNow = true,
-    rating = 4,
+    rating = 5,
     isClosedTemporarily,
   } = restaurant;
+
+  // การสร้างอาร์เรย์เพื่อเก็บจำนวนภาพ star SVG โดยจำนวนภาพที่จะเก็บนั้นอ้างอิงที่ตัวแปร rating ซึ่งมีจำนวนเท่ากับ 5 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <>
@@ -46,6 +59,14 @@ export const RestaurantInfoCard = ({ ...restaurant }) => {
         <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
         <Info>
           <Title>{name}</Title>
+
+          {/* ratingArray.map การวนลูป */}
+          <Rating>
+            {ratingArray.map((_, index) => (
+              <SvgXml key={index} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+
           <Address>{address}</Address>
         </Info>
       </RestaurantCard>
